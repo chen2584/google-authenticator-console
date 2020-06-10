1f3c2e7047667db90b44f1a1979f1a12718ba3ad
Worameth Semapat <worameth.semapat@gmail.com>
2020-06-10 14:17:58 +0700

    first commit

---
+++ src/GoogleAuthenticatorConsole/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GoogleAuthenticatorConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var googleAuthenticator = new GoogleAuthenticator();
+            //Console.WriteLine(googleAuthenticator.GenerateSecretKey(10));
+
+            Console.Write("Input SecretKey: ");
+            var secretKeyInput = Console.ReadLine();
+
+            Console.Write("Input Pin: ");
+            var pinInput = Console.ReadLine();
+
+            var googleAuthenticator = new GoogleAuthenticator();
+            var currentPin = googleAuthenticator.GeneratePin(secretKeyInput);
+
+            Console.WriteLine($"\nIs SecretKey: {secretKeyInput} has current PIN: {pinInput}?");
+            Console.WriteLine($"Result: {pinInput == currentPin}. (Current Pin: {currentPin})");
+        }
+    }
+}
